66813734b4233b12623bc4c2841e0a7acfb9de89
Isak Skoog <skoog.isak@gmail.com>
2025-07-23 16:43:45 +0200

    files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  darkMode: 'class',
+  plugins: [],
+}
